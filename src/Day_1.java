6c032ee0a8a51d804ec8aa84910b538e590dd448
pdobariya <punam.dobariya54@gmail.com>
2018-02-04 13:05:16 +0000

    First commit added

---
+++ src/Day_1.java
@@ -0,0 +1,11 @@
+public class Day_1 {
+    public static void main(String[] args) {
+        String greeting = "Hi Hello How are you";
+        System.out.print(greeting);
+        System.out.print(" There");
+        //System.out.println("Hello");
+        System.out.print("Sky");
+        int x; //int y;
+    }
+
+}
